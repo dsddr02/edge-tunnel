1956445f9d31cf00545b9ef8172b66de428c5ed9
dsddr02 <fldz5555a@outlook.com>
2025-02-26 01:56:24 +0800

    Update convert_csv_to_tls.py

--- convert_csv_to_tls.py
+++ convert_csv_to_tls.py
@@ -8,7 +8,7 @@ def convert_csv_to_tls(csv_filename, output_filename):
 
     with open(output_filename, 'w', encoding='utf-8') as f:
         for i, (ip, speed) in enumerate(zip(ips, download_speeds)):
-            f.write(f"{ip}#香港优选IP {i+1}  ↓ {speed}MB/s\n")
+            f.write(f"{ip}:8443#香港优选IP {i+1}  ↓ {speed}MB/s\n")
 
 csv_filename = 'result.csv'
 output_filename = 'HK.txt'
